339d5f7646a47f3088807501c8c771eaad3f8ada
Ruyuf <reyofalzidane@gmail.com>
2026-06-03 11:09:20 +0300

    Update vendor_lookup.c

--- vendor_lookup.c
+++ vendor_lookup.c
@@ -82,7 +82,7 @@ const char *lookup_vendor_by_mac(const uint8_t mac[6]) {
 
 void load_oui_database(const char *filename) {
     FILE *file = fopen(filename, "r");
-    printf("[DEBUG] Loaded OUI: '%s' → '%s'\n", oui_list[oui_count].prefix, oui_list[oui_count].vendor);
+    //-printf("[DEBUG] Loaded OUI: '%s' → '%s'\n", oui_list[oui_count].prefix, oui_list[oui_count].vendor);
 
     if (!file) {
         perror("Failed to open OUI database");
@@ -97,7 +97,7 @@ void load_oui_database(const char *filename) {
             *comma = '\0';
             strncpy(oui_list[oui_count].prefix, line, sizeof(oui_list[oui_count].prefix));
             strncpy(oui_list[oui_count].vendor, comma + 1, sizeof(oui_list[oui_count].vendor));
-            printf("[DEBUG] Loaded OUI: %s → %s\n", oui_list[oui_count].prefix, oui_list[oui_count].vendor);//hereee
+            //-printf("[DEBUG] Loaded OUI: %s → %s\n", oui_list[oui_count].prefix, oui_list[oui_count].vendor);//hereee
             oui_count++;
         }
     }
